8f72490a0d67563e5a1dfa91faaa998d3f28c933
Александр Донцов <dontsov.aa@reliab.tech>
2021-08-23 21:54:05 +0300

    fix: maybe so

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 )
 
 func main() {
-	var current, next, path = 0, 1, 0
+	var current, next, path = 1, 1, 1
 	var max = inputMax()
 
+	fmt.Print(strconv.Itoa(current) + " ")
 	for path < max {
 		fmt.Print(strconv.Itoa(path) + " ")
 		path = current + next
